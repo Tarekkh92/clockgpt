b3109380e9b02e0f037b7b63a9e7d52a2ede0692
fardos <khalailyt@gmail.com>
2023-05-22 15:59:43 +0300

    first commit

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function updateTime() {
   var egyptTime = now.toLocaleString("en-EG", { timeZone: "Africa/Cairo" });
   var syriaTime = now.toLocaleString("en-SY", { timeZone: "Asia/Damascus" });
   var lebanonTime = now.toLocaleString("en-LB", { timeZone: "Asia/Beirut" });
+  var spainTime = now.toLocaleString("en-LB", { timeZone: "Europe/Madrid" });
+  var italyTime = now.toLocaleString("en-LB", { timeZone: "Europe/Rome" });
+  var germanyTime = now.toLocaleString("en-LB", { timeZone: "Europe/Berlin" });
+
+  var moroccoTime = now.toLocaleString("en-LB", { timeZone: "Africa/Casablanca" });
+  var australiaTime = now.toLocaleString("en-LB", { timeZone: "Australia/Sydney" });
+  var saTime = now.toLocaleString("en-LB", { timeZone: "Africa/Johannesburg" });
+  var canadaTime = now.toLocaleString("en-LB", { timeZone: "America/Toronto" });
+
+
+  var brazilTime = now.toLocaleString("en-LB", { timeZone: "America/Sao_Paulo"});
+  var argentinaTime = now.toLocaleString("en-LB", { timeZone: "America/Argentina/Buenos_Aires"});
+  var japanTime = now.toLocaleString("en-LB", { timeZone: "Asia/Tokyo"});
+  var colombiaTime = now.toLocaleString("en-LB", { timeZone: "America/Bogota"});
 
   // Update the digital clocks
   document.getElementById("london-digital-clock").textContent = londonTime;
@@ -22,17 +36,20 @@ function updateTime() {
   document.getElementById("egypt-digital-clock").textContent = egyptTime;
   document.getElementById("syria-digital-clock").textContent = syriaTime;
   document.getElementById("lebanon-digital-clock").textContent = lebanonTime;
+  document.getElementById("spain-digital-clock").textContent = spainTime;
+  document.getElementById("italy-digital-clock").textContent = italyTime;
+  document.getElementById("germany-digital-clock").textContent = germanyTime;
 
+  document.getElementById("morocco-digital-clock").textContent = moroccoTime;
+  document.getElementById("australia-digital-clock").textContent = australiaTime;
+  document.getElementById("sa-digital-clock").textContent = saTime;
+  document.getElementById("canada-digital-clock").textContent = canadaTime;
+  document.getElementById("brazil-digital-clock").textContent = brazilTime;
+  document.getElementById("argentina-digital-clock").textContent = argentinaTime;
+  document.getElementById("japan-digital-clock").textContent = japanTime;
+  document.getElementById("colombia-digital-clock").textContent = colombiaTime;
   // Update the analog clocks
-  updateAnalogClock("london-analog-clock", now, "Europe/London");
-  updateAnalogClock("usa-analog-clock", now, "America/New_York");
-  updateAnalogClock("palestine-analog-clock", now, "Asia/Gaza");
-  updateAnalogClock("india-analog-clock", now, "Asia/Kolkata");
-  updateAnalogClock("pakistan-analog-clock", now, "Asia/Karachi");
-  updateAnalogClock("jordan-analog-clock", now, "Asia/Amman");
-  updateAnalogClock("egypt-analog-clock", now, "Africa/Cairo");
-  updateAnalogClock("syria-analog-clock", now, "Asia/Damascus");
-  updateAnalogClock("lebanon-analog-clock", now, "Asia/Beirut");
+
 }
 
 
